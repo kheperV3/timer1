ea4bdf38a848e47c18d2938fd88733990dcde545
kheperV3 <louis@ros.fr>
2018-06-09 17:15:07 +0200

    Update action-louisros.Timer.py

--- action-louisros.Timer.py
+++ action-louisros.Timer.py
@@ -51,4 +51,4 @@ def action_wrapper(hermes, intentMessage, conf):
 if __name__ == "__main__":
     with Hermes("localhost:1883") as h:
         h.subscribe_intent("louisros:settimer", subscribe_intent_callback) \
-         .start()
+         .loop_forever()
